93471aac7c3af34febd401b50f6bf5dcf6f6422e
Reginaldo Junior <reginaldo@deltacoding.xyz>
2017-11-12 10:18:19 -0200

    Updated create_users_table and User model

--- domains/Acl/routes/api.php
+++ domains/Acl/routes/api.php
@@ -1,11 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/token/{user}', function (\App\User $user) {
-    return $user;
-});
-
-Route::middleware('auth')->get('/info', function () {
-
-});
