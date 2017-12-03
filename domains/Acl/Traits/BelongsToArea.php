f47dfaad1e5f71dd1c171d8dc2a3955a8d414824
Reginaldo Junior <reginaldo@deltacoding.xyz>
2017-12-03 13:45:25 -0200

    Removed unused methods from BelongsToArea trait

--- domains/Acl/Traits/BelongsToArea.php
+++ domains/Acl/Traits/BelongsToArea.php
@@ -11,27 +11,6 @@ namespace Acl\Traits;
 
 trait BelongsToArea
 {
-
-   /* public static function createPath()
-    {
-        return route('Acl::create-users.store');
-    }
-
-    public static function readPath()
-    {
-        return route('Acl::read-users.read');
-    }
-
-    public function getUpdatePathAttribute()
-    {
-        return route('Acl::update-users.update', $this);
-    }
-
-    public function getDeletePathAttribute()
-    {
-        return route('Acl::delete-users.delete', $this);
-    }*/
-
     public function getAdminAreaPathAttribute()
     {
         switch ($this->role) {
